bb9a5c6f854476b05580f9b204ab30577b3b7e6a
AestheticEssence <deploy@aestheticessence.com.np>
2026-09-11 04:40:18 +0545

    more review profiles

--- src/features/home/data/testimonialsData.ts
+++ src/features/home/data/testimonialsData.ts
@@ -25,8 +25,7 @@ export const testimonials: TestimonialItem[] = [
     text: "I recently had treatment here and it was a great experience. The staff was very welcoming, attentive, and made sure I was comfortable throughout my visit. They were polite, well-organized, and explained each step of the process clearly. The dermatologist was professional, kind, and took the time to answer all my questions. My skin has already started to show improvement. Highly recommend this clinic for both the treatment quality and the excellent service!",
     rating: 5,
     sourceUrl: "https://maps.app.goo.gl/3nBAmTJJjJy9y2eE6",
-    imageSrc:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=200&h=200&auto=format&fit=crop",
+    imageSrc: "/public/roshani.webp",
     avatarBg: "from-amber-400 to-yellow-600",
   },
   {
@@ -34,18 +33,15 @@ export const testimonials: TestimonialItem[] = [
     text: "I have been doing this skin scar removal treatment for about 4 months now, and honestly it is going really well. I can clearly see improvement in my skin, and the scars have started fading. The process has been comfortable too. I’m really happy with the results so far and glad I went for it",
     rating: 5,
     sourceUrl: "https://maps.app.goo.gl/6A9qhzAJAervZaqr7",
-    imageSrc:
-      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=200&h=200&auto=format&fit=crop",
+    imageSrc: "/public/arjun-patel.webp",
     avatarBg: "from-yellow-500 to-amber-700",
   },
   {
     name: "Purnima KC",
     text: "One of the Best skin clinic I have visited.The staffs are knowledgeable, and the equipment is top-notch👌🥰Highly recommend  for anyone looking for high-quality skincare.",
     rating: 5,
-    sourceUrl:
-      "https://maps.app.goo.gl/YV93VYLTC8No1nAy7https://maps.app.goo.gl/YV93VYLTC8No1nAy7",
-    imageSrc:
-      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=200&h=200&auto=format&fit=crop",
+    sourceUrl: "https://maps.app.goo.gl/YV93VYLTC8No1nAy7",
+    imageSrc: "/public/purnima.webp",
     avatarBg: "from-yellow-400 to-amber-600",
   },
   {
@@ -53,8 +49,7 @@ export const testimonials: TestimonialItem[] = [
     text: "The staff are very professional and accommodating, and the clinic is very clean and aesthetically beautiful inside. Dr Shraddha is very knowledgable and I felt very comfortable with her as she explains everything very throughly. Highly recommend!",
     rating: 5,
     sourceUrl: "https://maps.app.goo.gl/Kg4RqNtTev3mGvNJ8",
-    imageSrc:
-      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=200&h=200&auto=format&fit=crop",
+    imageSrc: "manisha.webp",
     avatarBg: "from-amber-400 to-orange-600",
   },
 ];
